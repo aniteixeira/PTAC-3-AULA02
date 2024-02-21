c63910fa0fd425e6cdac66e34ad70e4e4df1707e
aniteixeira <lisagoncalves.1977@gmail.com>
2024-02-21 14:11:54 -0400

    Atualizaçãio 21/02

---
+++ src/app/componentes/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return(
+        <footer>
+          <h1>Footer</h1>
+          <nav>
+           <ul>
+             <li>Home</li>
+            <li>Registrar</li>
+           </ul>
+          </nav>
+        </footer>
+    );
+}
