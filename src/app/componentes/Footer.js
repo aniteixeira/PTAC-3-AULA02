99302365041ad53e8f292815e286b6305d42ad5b
aniteixeira <lisagoncalves.1977@gmail.com>
2024-03-06 12:05:38 -0400

    Atualização 06.03

--- src/app/componentes/Footer.js
+++ src/app/componentes/Footer.js
@@ -1,13 +1,27 @@
-export default function Footer(){
+import Link from "next/link";
+import Image from "next/image";
+import Styles from "./footer.module.css";
+
+export default function footer(){
     return(
-        <footer>
-          <h1>Footer</h1>
+        <footer className={Styles.footer}>
+          
           <nav>
-           <ul>
-             <li>Home</li>
-            <li>Registrar</li>
-           </ul>
-          </nav>
+            <li>
+          <Image 
+          width={100}
+          height={100}
+          src={"https://moodle.org/theme/moodleorg/pix/moodle_logo_TM.svg"}/>
+             </li>
+             <ul>
+              <Link href="/">
+                <li>Home</li>
+              </Link>
+              <Link href="/registro">
+                <li>Registrar</li>
+              </Link>
+             </ul>
+           </nav>
         </footer>
     );
-}
+};
