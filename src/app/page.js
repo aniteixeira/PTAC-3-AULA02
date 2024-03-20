ba3b06bf77d054fa373af9681c1338c87f6c77d3
aniteixeira <lisagoncalves.1977@gmail.com>
2024-03-20 14:17:33 -0400

    Atualização 20.03

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
-
 export default async function Home() {
 
-  return (
-    <main>
-      <h1>Home</h1>
-    </main>
-  )
-}
+const url = "https://back-end-ifms-one.vercel.app/campi";
+  
+    const resposta = await fetch(url,{
+      method: 'GET'
+    });
+    const campi = await resposta.json();
+   
+    return (
+      <main>
+        <h1>Home</h1>
+        {campi.map((campus) =>
+          <div>
+               <p>{campus.nome_campus}</p>
+          </div>
+        )}
+      </main>
+    )
+  };
